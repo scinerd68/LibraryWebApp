919e1b54ae6b1e73bf2cdeb70e81e559d49aa1bf
Phan Thang <thangphan0168@gmail.com>
2022-01-21 11:13:27 +0700

    Migrate to Postgresql

--- library/config.py
+++ library/config.py
@@ -1,6 +1,11 @@
 import os
 
+POSTGRES_URL = os.environ.get("POSTGRES_URL")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PW = os.environ.get("POSTGRES_PW")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
 
 class Config:
     SECRET_KEY = os.environ.get("LIBRARY_WEB_APP")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("LIBRARY_DB")
+    SQLALCHEMY_DATABASE_URI = DB_URL
