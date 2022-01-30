74779bdd5d3e36832456593f40d82ade6f43469d
Phan Thang <thangphan0168@gmail.com>
2022-01-30 16:48:04 +0700

    Add length validator to title, category, description field in InsertBookForm

--- library/books/forms.py
+++ library/books/forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, SubmitField, IntegerField, FieldList, TextAreaField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import DataRequired, NumberRange, Length
+
 
 class InsertBookForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired()])
-    category = StringField("Category", validators=[DataRequired()])
+    title = StringField("Title", validators=[DataRequired(), Length(min=1, max=100)])
+    category = StringField("Category", validators=[DataRequired(), Length(min=1, max=100)])
     authors = FieldList(StringField("Author"), min_entries=3, max_entries=3)
-    description = TextAreaField('Description', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired(), Length(min=1, max=500)])
     added_quantity = IntegerField("Number of books to add", validators=[NumberRange(min=0)])
     remove_quantity = IntegerField("Number of books to remove", validators=[NumberRange(min=0)])
     image = FileField('Update Book Cover', validators=[FileAllowed(['jpg', 'png'])])
